599ccc4ea562fc3dee608399ae611fe2f5fa00b8
asreed666 <areed@cityplym.ac.uk>
2024-01-03 09:48:00 +0000

    test

--- main.cpp
+++ main.cpp
@@ -10,14 +10,23 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS                                                   100
 
+#include "stdio.h"
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+    DigitalOut vcc(P10_3);
+    DigitalOut gnd(P10_0);
+    AnalogIn therm(P10_1);
+    printf("Testing\n");
+    vcc = 1;
+    gnd = 0;
 
     while (true) {
         led = !led;
+        float temp = therm.read();
+        printf("the voltage is: %f\n", temp );
         thread_sleep_for(BLINKING_RATE_MS);
     }
 }
